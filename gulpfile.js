6440916095315e0fa2a02c6705c28ce74fe72d2f
Matija Marohnić <matija.marohnic@gmail.com>
2014-12-05 15:10:39 +0100

    Responsive fixes

--- gulpfile.js
+++ gulpfile.js
@@ -50,10 +50,10 @@ gulp.task('html', ['views', 'styles'], function () {
 
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
+    // .pipe($.cache($.imagemin({
+    //   progressive: true,
+    //   interlaced: true
+    // })))
     .pipe(gulp.dest('dist/images'));
 });
 
